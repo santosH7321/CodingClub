fe3080ca4edc98767b948ec394fdb0092c16182c
Santosh Kumar <santoshkumar23kky@gmail.com>
2023-10-10 16:46:14 +0530

    ReduxSetUp

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import store from './Redux/Slices/Store.js';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import { Toaster } from 'react-hot-toaster';
+import { Provider } from 'react-redux';
+import { Toaster } from 'react-hot-toast';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store} >
   <BrowserRouter>
     <App />
     <Toaster />
   </BrowserRouter>
+  </Provider>
 );
