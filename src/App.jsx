935fdf0399a5cec865be959b9d898c61e1a83909
Santosh Kumar <santoshkumar23kky@gmail.com>
2023-10-10 18:16:31 +0530

    HomeLayOut

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import { Routes } from 'react-router-dom';
 import './App.css'
 import Footer from './Components/Footer';
 
+import HomePage from './Pages/HomePage';
+
 function App() {
 
   return (
     <>
-      {/* <Routes> */}
-           
-      {/* </Routes> */}
-      <Footer />
+      <Routes>
+           <Route path="/" element={ <HomePage />} ></Route>
+      </Routes>
+     
 
     </>
   ); 
